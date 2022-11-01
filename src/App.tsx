2125e7444ba45efe62b7b8794a5961b8a7adc58a
dhe19 <info.die@gmail.com>
2022-11-01 01:59:26 -0600

    add react-helmet package, and setup Helmet Provide and Helmet componet

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { SEO } from './components/SEO';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <SEO
+      title = 'Learning React Helmet'
+      description='Beginner friendly page for learning React Helemt'
+      name='Impulsora Valshoy'
+      type='article'/>
+      <div className="App">        
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+              Learn more about React Helmet <a href="https://www.freecodecamp.org/news/react-helmet-examples/">Here</a>
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </React.Fragment>
   );
 }
 
