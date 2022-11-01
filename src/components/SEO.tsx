dde444ff07a00e16cf5401e280de9345613e5c59
dhe19 <info.die@gmail.com>
2022-11-01 03:50:16 -0600

    fix a little problem

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -21,7 +21,7 @@ export const SEO:React.FC<Props> = ({title, description, name, type}) =>{
             <meta property="og:description" content={description} />
             { /* End Facebook tags */ }
             { /* Twitter tags */ }
-            <meta name="twitter:creator" content={name} />}
+            <meta name="twitter:creator" content={name} />
             <meta name="twitter:card" content={type} />
             <meta name="twitter:title" content={title} />
             <meta name="twitter:description" content={description} />
